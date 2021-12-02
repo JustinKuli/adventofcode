2cbe98eee3bb8e0634cd8a11fb391595c8db30cf
Justin Kulikauskas <44813129+JustinKuli@users.noreply.github.com>
2021-12-02 01:33:41 -0500

    Update templates
    The script will now download the data file, and the code scaffold is a
    bit more useful.
    
    Signed-off-by: Justin Kulikauskas <justin.kulikauskas@gmail.com>

--- start-puzzle.sh
+++ start-puzzle.sh
@@ -4,9 +4,17 @@ set -euxo pipefail
 
 day=$1
 wd="./days/${day}"
+saniday=$(echo ${day} | sed -z 's/^0//g') # remove leading 0 if present
 
 cargo new --vcs "none" --name "aoc_2021_${day}" "${wd}"
-touch "${wd}/data.txt"
+
+curl "https://adventofcode.com/2021/day/${saniday}/input" \
+  -H 'pragma: no-cache' \
+  -H 'cache-control: no-cache' \
+  -H 'user-agent: idk' \
+  -H "$(cat cookie-header.txt)" \
+  -H 'accept: text/html' > "${wd}/data.txt"
+
 cp ./template/main.rs "${wd}/src/main.rs"
 cp ./template/format.sh "${wd}/format.sh"
 cat ./template/initial-deps.txt >> "${wd}/Cargo.toml"
--- template/main.rs
+++ template/main.rs
@@ -1,18 +1,30 @@
 fn main() {
-    let data = read_data(String::from("formatted.txt"))
+    let data = read_data(String::from("data.txt"))
         .expect("Failed to get data");
 
-    dbg_print_data(data)
+    part_one(data.clone());
+    part_two(data.clone());
 }
 
-fn dbg_print_data(data: Vec<csv::StringRecord>) {
-    let mut i = 0;
+fn part_one(data: Vec<csv::StringRecord>) {
+    let acc = 0;
+    let mut v = Vec::new();
+
     for line in data {
         for item in line.into_iter() {
-            println!("Line: {} \t Item: {}", i, item)
+            let val = str::parse::<i32>(item)
+                .expect("Or else");
+            v.push(val)
         }
-        i += 1;
     }
+
+    println!("part one: {}", acc)
+}
+
+fn part_two(data: Vec<csv::StringRecord>) {
+    let acc = 0;
+
+    println!("part two: {}", acc)
 }
 
 fn read_data(path: String) -> Result<Vec<csv::StringRecord>, Box<dyn std::error::Error>> {
