f4b0350aec7791a00c2bd1c7142d3608e228aad9
Justin Kulikauskas <jkulikau@redhat.com>
2021-12-03 14:30:18 -0500

    Update template
    Adds more boilerplate to main.rs, both for possible use directly, and
    as example code to copy and modify.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- start-puzzle.sh
+++ start-puzzle.sh
@@ -18,6 +18,7 @@ curl "https://adventofcode.com/2021/day/${saniday}/input" \
 cp ./template/main.rs "${wd}/src/main.rs"
 cp ./template/format.sh "${wd}/format.sh"
 cat ./template/initial-deps.txt >> "${wd}/Cargo.toml"
+touch "${wd}/small-data.txt"
 
 cd "${wd}"
 cargo build
--- template/format.sh
+++ template/format.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# spaces to commas:
+# This "script" can be used to clean up the puzzle inputs for easier handling
+
+# spaces to commas (useful if sets of information are stored on a line):
 sed -z 's/ /,/g' data.txt > formatted.txt
 
-# single newline to comma, double newline to single, spaces to commas:
+# # single newline to comma, double newline to single newline
+# # (useful if sets of data are stored on consecutive lines):
 # sed -z 's|\n\n|¯\_(ツ)_/¯|g' data.txt > formatted.txt
 # sed -i -z -e 's|\n|,|g' formatted.txt
 # sed -i -z -e 's|¯\_(ツ)_/¯|\n|g' formatted.txt
-# sed -i -z -e 's/ /,/g' formatted.txt
