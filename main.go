b9c03c362d1affad289e9e5d57bd807e90a068b7
Justin Kulikauskas <jkulikau@redhat.com>
2022-12-19 18:40:50 -0500

    Day 19 part one
    Is it just me, or are the second parts getting harder(er)?
    
    I wasn't sure if this "best known" thing would help very much, but it
    improves the performance on the sample from about 60s to 40s! Still not
    as fast as I would want for part 1 on the actual input, and definitely
    not good enough for part 2.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/JustinKuli/aoc2022/day15"
+	"github.com/JustinKuli/aoc2022/day19"
 )
 
 func main() {
-	day15.Run("sample", "./day15/sample.txt")
-	day15.Run("real", "./day15/input.txt")
+	day19.Run("sample", "./day19/sample.txt")
+	// day19.Run("real", "./day19/input.txt")
 }
