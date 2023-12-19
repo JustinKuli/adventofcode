f993d5fffc5bf5f83579e9590dbc0dce34744b7a
Justin Kulikauskas <jkulikau@redhat.com>
2023-12-18 22:15:40 -0500

    Day 17 complete; p2 surprisingly easy?

--- 2023-nodejs/day17/day.js
+++ 2023-nodejs/day17/day.js
@@ -2,114 +2,285 @@
 
 const fs = require('node:fs');
 
-fs.readFile('./sample.txt', (err, data) => {
-// fs.readFile('./input.txt', (err, data) => {
+// fs.readFile('./sample.txt', (err, data) => {
+fs.readFile('./input.txt', (err, data) => {
     if (err) {
         console.error(err);
     }
 
+    let map = []
     const lines = data.toString().split('\n')
-
-    let graph = []
-    let unvisited = {}
-    let tentative_bydist = {} // key: distance, val: ["row,col,direction_in"]
-    let tentative_bykey = {} // key: "row,col,direction_in", val: distance
-
-    lines.forEach((line, row) => {
-        let graphline = []
-       
-        line.split("").forEach((n, col) => {
-            graphline.push(Number(n))
-            let key = row + "," + col
-            // visiting horizontally and vertically are different
-            unvisited[key+",H"] = true // can use `"x,y" in unvisited` OR `unvisited["x,y"]`
-            unvisited[key+",V"] = true // can use `"x,y" in unvisited` OR `unvisited["x,y"]`
-        })
-        graph.push(graphline)
+    lines.forEach((line) => {
+        map.push(line.split("").map((n) => Number(n)))
     })
 
-    delete unvisited["0,0,H"]
-    delete unvisited["0,0,V"]
+    // pre-populating with the possible future ways into 0,0 so they're skipped
+    let visited = {
+        // Part One
+        // "0,0,<": {path: "", loss: 0},
+        // "0,0,<<": {path: "", loss: 0},
+        // "0,0,<<<": {path: "", loss: 0},
+        // "0,0,^": {path: "", loss: 0},
+        // "0,0,^^": {path: "", loss: 0},
+        // "0,0,^^^": {path: "", loss: 0},
 
-    let curr = {row: 0, col: 0, dist: 0, direction: ""}
+        // Part Two
+        "0,0,<<<<": {path: "", loss: 0},
+        "0,0,<<<<<": {path: "", loss: 0},
+        "0,0,<<<<<<": {path: "", loss: 0},
+        "0,0,<<<<<<<": {path: "", loss: 0},
+        "0,0,<<<<<<<<": {path: "", loss: 0},
+        "0,0,<<<<<<<<<": {path: "", loss: 0},
+        "0,0,<<<<<<<<<<": {path: "", loss: 0},
+        "0,0,^^^^": {path: "", loss: 0},
+        "0,0,^^^^^": {path: "", loss: 0},
+        "0,0,^^^^^^": {path: "", loss: 0},
+        "0,0,^^^^^^^": {path: "", loss: 0},
+        "0,0,^^^^^^^^": {path: "", loss: 0},
+        "0,0,^^^^^^^^^": {path: "", loss: 0},
+        "0,0,^^^^^^^^^^": {path: "", loss: 0},
+    }
 
-    getNeighbors(curr, 3, graph).forEach((node) => {
-        let nodekey = node.row + "," + node.col + "," + node.direction
-        if (unvisited[nodekey]) {
-            if (nodekey in tentative_bykey) {
-                let prevdist = tentative_bykey[nodekey]
-                if (prevdist < node.dist) {
-                    tentative_bykey[nodekey] = node.dist
+    let tentByLoss = {} // like "4": ["0,1,>"]
+    let tentByKey = {} // like "0,1,>" : {row: 0, col: 1, loss: 4, path: ">"}
 
-                    let idx = tentative_bydist[prevdist].indexOf(nodekey)
-                    tentative_bydist[prevdist].splice(idx, 1)
+    let curr = {row: 0, col: 0, loss: 0, path: ""}
 
-                    if (node.dist in tentative_bydist) {
-                        tentative_bydist[node.dist].push(nodekey)
+    while (curr.row !== map.length-1 || curr.col !== map[0].length-1) {
+        // look at neighbors, compare to tentatives and update as necessary
+        neighbors2(curr, map).forEach((n) => {
+            let end = lastSegment(n.path)
+            let key = n.row + "," + n.col + "," + end
+            if (key in visited) {
+                return
+            }
+
+            if (key in tentByKey) {
+                let existingLoss = tentByKey[key].loss
+                if (n.loss < existingLoss) {
+                    tentByKey[key] = n
+
+                    let idxToRemove = tentByLoss[existingLoss].indexOf(key)
+                    tentByLoss[existingLoss].splice(idxToRemove, 1)
+                    if (tentByLoss[existingLoss].length === 0) {
+                        delete tentByLoss[existingLoss]
+                    }
+
+                    if (n.loss in tentByLoss) {
+                        tentByLoss[n.loss].push(key)
                     } else {
-                        tentative_bydist[node.dist] = [nodekey]
-                    }   
+                        tentByLoss[n.loss] = [key]
+                    }
                 }
             } else {
-                tentative_bykey[nodekey] = node.dist
+                tentByKey[key] = n
 
-                if (node.dist in tentative_bydist) {
-                    tentative_bydist[node.dist].push(nodekey)
+                if (n.loss in tentByLoss) {
+                    tentByLoss[n.loss].push(key)
                 } else {
-                    tentative_bydist[node.dist] = [nodekey]
-                }   
+                    tentByLoss[n.loss] = [key]
+                }
+            }
+        })
+
+        // mark curr as visited; remove it from tentatives
+        let currKey = curr.row + "," + curr.col + "," + lastSegment(curr.path)
+        visited[currKey] = {path: curr.path, loss: curr.loss}
+        if (currKey in tentByKey) {
+            delete tentByKey[currKey]
+
+            let idxToRemove = tentByLoss[curr.loss].indexOf(currKey)
+            tentByLoss[curr.loss].splice(idxToRemove, 1)
+            if (tentByLoss[curr.loss].length === 0) {
+                delete tentByLoss[curr.loss]
             }
         }
-    })
 
-    let currkey = curr.row + "," + curr.col + "," + curr.direction
-    delete unvisited[currkey]
+        // Get next smallest tentative to check next
+        let tentLosses = Object.keys(tentByLoss).map((l) => Number(l))
+        let smallestLoss = tentLosses[0]
+        tentLosses.forEach((l) => {
+            if (l < smallestLoss) {
+                smallestLoss = l
+            }
+        })
 
-    // TODO: go through tentatives, find smallest distance one, set curr to it.
-    // TODO: figure out the loop break-out conditions
+        let nextKey = tentByLoss[smallestLoss][0]
+        curr = tentByKey[nextKey]
+    }
 
-    console.log("Part One")
+    console.log(curr)
 });
 
-function getNeighbors(curr, maxStraight, graph) {
-    let neighbors = []
-    if (curr.direction === "V" || curr.direction === "") {
-        // add left neighbors
-        let leftdist = curr.dist
-        for (let col = curr.col - 1; col >= curr.col - maxStraight; col--) {
-            if (col >= 0 && col < graph.length) {
-                leftdist += graph[curr.row][col]
-                neighbors.push({row: curr.row, col: col, direction: "H", dist: leftdist})
+
+// Part One
+function neighbors1(curr, map) {
+    let end = lastSegment1(curr.path)
+    let ans = []
+
+    // Up and Down
+    if (end.length === 0 || end[0] === "<" || end[0] === ">") {
+        let upLoss = curr.loss
+        let downLoss = curr.loss
+        for (let i=1; i<=3; i++) {
+            upLoss += map[curr.row - i]?.[curr.col]
+            if (upLoss) {
+                ans.push({
+                    row: curr.row - i,
+                    col: curr.col,
+                    path: curr.path + "^".repeat(i),
+                    loss: upLoss    
+                })
             }
-        }
-        // add right neighbors
-        let rightdist = curr.dist
-        for (let col = curr.col + 1; col <= curr.col + maxStraight; col++) {
-            if (col >= 0 && col < graph.length) {
-                rightdist += graph[curr.row][col]
-                neighbors.push({row: curr.row, col: col, direction: "H", dist: rightdist})
+
+            downLoss += map[curr.row + i]?.[curr.col]
+            if (downLoss) {
+                ans.push({
+                    row: curr.row + i,
+                    col: curr.col,
+                    path: curr.path + "v".repeat(i),
+                    loss: downLoss    
+                })
             }
         }
     }
 
-    if (curr.direction === "H" || curr.direction === "") {
-        // add up neighbors
-        let updist = curr.dist
-        for (let row = curr.row - 1; row >= curr.row - maxStraight; row--) {
-            if (row >=0 && row < graph.length) {
-                updist += graph[row][curr.col]
-                neighbors.push({row: row, col: curr.col, direction: "V", dist: updist})
+    // Left and Right
+    if (end.length === 0 || end[0] === "v" || end[0] === "^") {
+        let leftLoss = curr.loss
+        let rightLoss = curr.loss
+        for (let i=1; i<=3; i++) {
+            leftLoss += map[curr.row][curr.col - i]
+            if (leftLoss) {
+                ans.push({
+                    row: curr.row,
+                    col: curr.col - i,
+                    path: curr.path + "<".repeat(i),
+                    loss: leftLoss    
+                })
             }
-        }
-        // add down neighbors
-        let downdist = curr.dist
-        for (let row = curr.row + 1; row <= curr.row + maxStraight; row++) {
-            if (row >=0 && row < graph.length) {
-                downdist += graph[row][curr.col]
-                neighbors.push({row: row, col: curr.col, direction: "V", dist: downdist})
+
+            rightLoss += map[curr.row][curr.col + i]
+            if (rightLoss) {
+                ans.push({
+                    row: curr.row,
+                    col: curr.col + i,
+                    path: curr.path + ">".repeat(i),
+                    loss: rightLoss
+                })
             }
         }
     }
 
-    return neighbors
+    return ans
+}
+
+function lastSegment1(path) {
+    const l = path.length
+    if (l === 0) {
+        return ""
+    }
+
+    let end = path[l-1]
+
+    if (path[l-2] === end) {
+        end += path[l-2]
+    } else {
+        return end
+    }
+
+    if (path[l-3] === path[l-2]) {
+        end += path[l-3]
+    }
+
+    return end
+}
+
+// Part Two
+function neighbors2(curr, map) {
+    let end = lastSegment(curr.path)
+    let ans = []
+
+    // Up and Down
+    if (end.length === 0 || end[0] === "<" || end[0] === ">") {
+        let upLoss = curr.loss
+        let downLoss = curr.loss
+        for (let i=1; i<=3; i++) {
+            upLoss += map[curr.row - i]?.[curr.col]
+            downLoss += map[curr.row + i]?.[curr.col]
+        }
+        for (let i=4; i<=10; i++) {
+            upLoss += map[curr.row - i]?.[curr.col]
+            if (upLoss) {
+                ans.push({
+                    row: curr.row - i,
+                    col: curr.col,
+                    path: curr.path + "^".repeat(i),
+                    loss: upLoss    
+                })
+            }
+
+            downLoss += map[curr.row + i]?.[curr.col]
+            if (downLoss) {
+                ans.push({
+                    row: curr.row + i,
+                    col: curr.col,
+                    path: curr.path + "v".repeat(i),
+                    loss: downLoss    
+                })
+            }
+        }
+    }
+
+    // Left and Right
+    if (end.length === 0 || end[0] === "v" || end[0] === "^") {
+        let leftLoss = curr.loss
+        let rightLoss = curr.loss
+        for (let i=1; i<=3; i++) {
+            leftLoss += map[curr.row][curr.col - i]
+            rightLoss += map[curr.row][curr.col + i]
+        }
+        for (let i=4; i<=10; i++) {
+            leftLoss += map[curr.row][curr.col - i]
+            if (leftLoss) {
+                ans.push({
+                    row: curr.row,
+                    col: curr.col - i,
+                    path: curr.path + "<".repeat(i),
+                    loss: leftLoss    
+                })
+            }
+
+            rightLoss += map[curr.row][curr.col + i]
+            if (rightLoss) {
+                ans.push({
+                    row: curr.row,
+                    col: curr.col + i,
+                    path: curr.path + ">".repeat(i),
+                    loss: rightLoss
+                })
+            }
+        }
+    }
+
+    return ans
+}
+
+// Just better
+function lastSegment(path) {
+    const l = path.length
+    if (l === 0) {
+        return ""
+    }
+
+    let end = path[l-1]
+    let prev = end
+
+    let i = 2
+    while(path[l-i] === prev) {
+        end += prev
+        i++
+    }
+
+    return end
 }
